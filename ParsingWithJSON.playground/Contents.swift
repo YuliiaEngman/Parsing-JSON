133869a90e30a9cdf49bcb1f853586591fca1750
Yuliia Engman <yuliia.engman@gmail.com>
2020-07-28 12:25:49 -0400

    recording JSON

--- ParsingWithJSON.playground/Contents.swift
+++ ParsingWithJSON.playground/Contents.swift
@@ -1,3 +1,47 @@
-import UIKit
+// Parsing Dictionary
+import Foundation
 
-var str = "Hello, playground"
+// JSON Data
+let json = """
+{
+ "results": [
+   {
+     "firstName": "John",
+     "lastName": "Appleseed"
+   },
+  {
+    "firstName": "Alex",
+    "lastName": "Paul"
+  }
+ ]
+}
+""".data(using: .utf8)!
+
+// Create Model(s)
+
+// Codable: Decodable & Encodable
+// Decodable: converts jsom data
+// Encodable: converts to jsom data to e.g. POST to a API
+
+
+// Top level JSON is a Dictionary
+struct ResultsWrapper: Decodable {
+    let results: [Contact]
+}
+
+struct Contact: Decodable {
+    let firstName: String
+    let lastName: String
+}
+
+// =============================
+// decode the JSON data to our Swift model
+//==============================
+
+do {
+    let dictionary = try JSONDecoder().decode(ResultsWrapper.self, from: json)
+    let contacts = dictionary.results // [Contact]
+    dump(contacts)
+} catch {
+    print("decoding error: \(error)")
+}
