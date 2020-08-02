f592bf2403caae457fbe02a79305870cf1a054ce
Yuliia Engman <yuliia.engman@gmail.com>
2020-08-01 23:54:29 -0400

    parsing multiple keys

---
+++ ParsingWithJSON.playground/Pages/Parsing-Multiple-Keys.xcplaygroundpage/Contents.swift
@@ -0,0 +1,122 @@
+import Foundation
+
+let json = """
+{
+    "Afpak": {
+        "id": 1,
+        "race": "hybrid",
+        "flavors": [
+            "Earthy",
+            "Chemical",
+            "Pine"
+        ],
+        "effects": {
+            "positive": [
+                "Relaxed",
+                "Hungry",
+                "Happy",
+                "Sleepy"
+            ],
+            "negative": [
+                "Dizzy"
+            ],
+            "medical": [
+                "Depression",
+                "Insomnia",
+                "Pain",
+                "Stress",
+                "Lack of Appetite"
+            ]
+        }
+    },
+    "African": {
+        "id": 2,
+        "race": "sativa",
+        "flavors": [
+            "Spicy/Herbal",
+            "Pungent",
+            "Earthy"
+        ],
+        "effects": {
+            "positive": [
+                "Euphoric",
+                "Happy",
+                "Creative",
+                "Energetic",
+                "Talkative"
+            ],
+            "negative": [
+                "Dry Mouth"
+            ],
+            "medical": [
+                "Depression",
+                "Pain",
+                "Stress",
+                "Lack of Appetite",
+                "Nausea",
+                "Headache"
+            ]
+        }
+    },
+    "Afternoon Delight": {
+        "id": 3,
+        "race": "hybrid",
+        "flavors": [
+            "Pepper",
+            "Flowery",
+            "Pine"
+        ],
+        "effects": {
+            "positive": [
+                "Relaxed",
+                "Hungry",
+                "Euphoric",
+                "Uplifted",
+                "Tingly"
+            ],
+            "negative": [
+                "Dizzy",
+                "Dry Mouth",
+                "Paranoid"
+            ],
+            "medical": [
+                "Depression",
+                "Insomnia",
+                "Pain",
+                "Stress",
+                "Cramps",
+                "Headache"
+            ]
+        }
+    }
+}
+""".data(using: .utf8)!
+
+// Model(s):
+
+struct Strain: Decodable {
+    let id: Int
+    let race: String
+    let flavors: [String]
+    let effects: [Sting: [String]]
+}
+
+// Decode JSON to Swift oblects:
+
+do {
+    let dictionary = try JSONDecoder().decode([String: Strain.self], from: json)
+    // use a for-loop to create [Strain] or use map {}
+    var strains = [Strain]()
+    for (_, value) in dictionary {
+        let strain = Strain(id: value.id,
+                            race: value.race,
+                            flavors: value.flavors,
+                            effects: value.effects)
+        strains.append(strain)
+    }
+    dump(strains)
+} catch {
+    print(error)
+}
+
+
